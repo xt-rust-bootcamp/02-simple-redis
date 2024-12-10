0ab0d55c4ac73089adfd54adae9adf9c751a3510
xtxper <taoiaox@gmail.com>
2024-12-10 23:05:16 +0800

    Revert "feature: support network layer & refactor"
    This reverts commit 00399e6a4c75a42bd39e06b20455010c6f7ad6ce.

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -29,14 +29,12 @@ pub trait CommandExecutor {
 }
 
 #[enum_dispatch(CommandExecutor)]
-#[derive(Debug)]
 pub enum Command {
     Get(Get),
     Set(Set),
     HGet(HGet),
     HSet(HSet),
     HGetAll(HGetAll),
-    Unrecognized(Unrecognized),
 }
 
 #[derive(Debug)]
@@ -68,21 +66,6 @@ pub struct HGetAll {
     key: String,
 }
 
-#[derive(Debug)]
-pub struct Unrecognized;
-
-impl TryFrom<RespFrame> for Command {
-    type Error = CommandError;
-    fn try_from(v: RespFrame) -> Result<Self, Self::Error> {
-        match v {
-            RespFrame::Array(array) => array.try_into(),
-            _ => Err(CommandError::InvalidCommand(
-                "Command must be an Array".to_string(),
-            )),
-        }
-    }
-}
-
 impl TryFrom<RespArray> for Command {
     type Error = CommandError;
     fn try_from(v: RespArray) -> Result<Self, Self::Error> {
@@ -93,18 +76,15 @@ impl TryFrom<RespArray> for Command {
                 b"hget" => Ok(HGet::try_from(v)?.into()),
                 b"hset" => Ok(HSet::try_from(v)?.into()),
                 b"hgetall" => Ok(HGetAll::try_from(v)?.into()),
-                _ => Ok(Unrecognized.into()),
+                _ => Err(CommandError::InvalidCommand(format!(
+                    "Invalid command: {}",
+                    String::from_utf8_lossy(cmd.as_ref())
+                ))),
             },
             _ => Err(CommandError::InvalidCommand(
                 "Command must have a BulkString as the first argument".to_string(),
             )),
         }
-    }
-}
-
-impl CommandExecutor for Unrecognized {
-    fn execute(self, _: &Backend) -> RespFrame {
-        RESP_OK.clone()
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 mod backend;
 mod cmd;
-mod network;
 mod resp;
 
 pub use backend::*;
 pub use cmd::*;
-pub use network::*;
 pub use resp::*;
